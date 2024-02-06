c9266134daff6f65bfe9ccf676bc95c5b8f5b7b9
kmseunh <tmdgus8779@gmail.com>
2024-02-06 23:52:33 +0900

    feat: create a svelte-accordion

---
+++ accordion/src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Accodian from './components/Accodian.svelte';
+</script>
+
+<Accodian />
---
+++ accordion/src/components/Accodian.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    interface AccodionItem {
+        title: string;
+        content: string;
+    }
+
+    let activeIndex: number = -1;
+
+    const accodionItem: AccodionItem[] = [
+        { title: 'Item 1', content: 'Content for Item 1' },
+        { title: 'Item 2', content: 'Content for Item 2' },
+        { title: 'Item 3', content: 'Content for Item 3' },
+    ];
+
+    const handleIndex = (index: number): void => {
+        activeIndex = activeIndex === index ? '-1' : index;
+    };
+</script>
+
+<div class="flex justify-center items-center h-screen bg-gray-900 text-white">
+    <div class="flex flex-col">
+        {#each accodionItem as item, index}
+            <div class="border border-gray-700 rounded m-2 w-52">
+                <div
+                    class="flex justify-between items-center p-2 bg-gray-800 text-white"
+                >
+                    <div>{item.title}</div>
+                    <button on:click={() => handleIndex(index)}
+                        >{activeIndex === index ? '-' : '+'}</button
+                    >
+                </div>
+                {#if activeIndex === index}
+                    <div class="bg-gray-900 text-white p-2">
+                        <p>{item.content}</p>
+                    </div>
+                {/if}
+            </div>
+        {/each}
+    </div>
+</div>
