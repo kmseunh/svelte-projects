f01ecb1156240bf0a082ef84b940b755a7ba4d0b
kmseunh <tmdgus8779@gmail.com>
2024-02-09 19:19:29 +0900

    feat: create a svelte-star-rating

---
+++ svelte-star-rating/src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    let rating: number = 0;
+    let clicked: boolean = false;
+
+    const setRating = (value: number) => {
+        if (!clicked) {
+            rating = value;
+        }
+    };
+
+    const handleClick = (star: number) => {
+        clicked = true;
+        rating = star;
+    };
+
+    const resetRating = (): void => {
+        if (!clicked) {
+            rating = 0;
+        }
+    };
+</script>
+
+<div class="flex justify-center items-center h-screen">
+    {#each Array.from({ length: 5 }, (_, index) => index + 1) as star}
+        <span
+            class="text-8xl text-gray-400 cursor-pointer transition-colors duration-300 hover:text-yellow-400"
+            on:mouseenter={() => setRating(star)}
+            on:mouseleave={resetRating}
+            on:click={() => handleClick(star)}
+        >
+            {star <= rating ? '★' : '☆'}
+        </span>
+    {/each}
+</div>
