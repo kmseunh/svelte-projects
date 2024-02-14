66ebb8820a25358bbd35b2b102543bfea54729f6
kmseunh <tmdgus8779@gmail.com>
2024-02-14 10:40:19 +0900

    feat: create a svelte-search-autocomplete

--- svelte-search-autocomplete/src/App.svelte
+++ svelte-search-autocomplete/src/App.svelte
@@ -1,33 +1,14 @@
 <script lang="ts">
-    import axios from 'axios';
     import Search from './components/Search.svelte';
     import PokemonDetails from './components/PokemonDetails.svelte';
 
-    const API_URL: string = 'https://pokeapi.co/api/v2/pokemon';
-
     let pokemonDetails: any;
-
-    const fetchPokemonDetails = async (name: string): Promise<void> => {
-        try {
-            const response = await axios.get(`${API_URL}/${name}`);
-            pokemonDetails = response.data;
-        } catch (error) {
-            console.error('Error fetching Pokemon details:', error);
-            pokemonDetails = null;
-        }
-    };
-
-    const handleSearch = async (query: string): Promise<void> => {
-        if (query.trim() !== '') {
-            await fetchPokemonDetails(query);
-        }
-    };
 </script>
 
 <div class="flex flex-col items-center justify-center h-screen">
     <h1 class="text-4xl font-bold my-4 text-center">Pokémon Search</h1>
 
-    <Search on:search={handleSearch} bind:pokemonDetails />
+    <Search bind:pokemonDetails />
 
     <PokemonDetails {pokemonDetails} />
 </div>
